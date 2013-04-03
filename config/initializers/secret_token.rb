bd104158a9b790037eae0b2d29ca5953c4e45710
Wesley Truong <imswimmingincash@Wesleys-MacBook-Pro.local>
2013-04-03 16:04:47 -0700

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '1977a8314630ec9978e536cfb9ff4fd64b58ef76bcfc37782f01f15b0f8ef2db33e819b7f03c48a48e4cb4d9dc18cfc74fa402da0e4b728fe6f528e3463ab95b'
+SampleApp::Application.config.secret_token = '42226a75ad5c21a362587314f171889aaa7d295e83333fe50c82de87f8e7ad00a7eb292a1844c5bfd7baabfae5c81f9c58c4beab0c87f86ee7352e443205bd00'
